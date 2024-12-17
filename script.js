c29e14357e33ca80827c81180d89994972e10db1
asantoshka <asantoshka@users.noreply.github.com>
2024-12-17 08:34:45 +0000

    deploy: d60fc6634b7bc3f5a27abd8728841823f7eca3e1

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll('.section');
+
+    window.addEventListener('scroll', () => {
+        sections.forEach(section => {
+            const sectionTop = section.getBoundingClientRect().top;
+            if (sectionTop < window.innerHeight - 100) {
+                section.classList.add('fade-in');
+            }
+        });
+    });
+});
